79c79ef73280a873313e72b705700d2b73c0571b
Zachary Jensen <mrzacharyjensen@gmail.com>
2024-07-02 21:56:41 +1200

    Add description to harpoon menu
    Add descriptions to harpoon

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -12,22 +12,34 @@ return {
 
     vim.keymap.set('n', '<C-h>', function()
       harpoon:list():add()
-    end)
+    end,
+    { desc = 'Add to harpoon' }
+    )
     vim.keymap.set('n', '<leader>i', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
-    end)
+    end,
+    { desc = 'Harpoon menu' }
+    )
 
     vim.keymap.set('n', '<C-j>', function()
       harpoon:list():select(1)
-    end)
+    end,
+    { desc = 'Switch to harpoon window 1' }
+    )
     vim.keymap.set('n', '<C-k>', function()
       harpoon:list():select(2)
-    end)
+    end,
+    { desc = 'Switch to harpoon window 2' }
+    )
     vim.keymap.set('n', '<C-l>', function()
       harpoon:list():select(3)
-    end)
+    end,
+    { desc = 'Switch to harpoon window 3' }
+    )
     vim.keymap.set('n', '<C-m>', function()
       harpoon:list():select(4)
-    end)
+    end,
+    { desc = 'Switch to harpoon window 4' }
+    )
   end,
 }
