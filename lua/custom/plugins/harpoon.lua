a416467fd4f45e270553a3a9782ce39397650281
Zachary Jensen <mrzacharyjensen@gmail.com>
2024-07-24 14:48:43 +1200

    Disable harpoon

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,45 +1,45 @@
 return {
-  'ThePrimeagen/harpoon',
-  branch = 'harpoon2',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-
-  -- local harpoon = require("harpoon")
-
-  -- harpoon:setup()
-  config = function()
-    local harpoon = require 'harpoon'
-    harpoon:setup()
-
-    vim.keymap.set('n', '<C-h>', function()
-      harpoon:list():add()
-    end,
-    { desc = 'Add to harpoon' }
-    )
-    vim.keymap.set('n', '<leader>i', function()
-      harpoon.ui:toggle_quick_menu(harpoon:list())
-    end,
-    { desc = 'Harpoon menu' }
-    )
-
-    vim.keymap.set('n', '<C-j>', function()
-      harpoon:list():select(1)
-    end,
-    { desc = 'Switch to harpoon window 1' }
-    )
-    vim.keymap.set('n', '<C-k>', function()
-      harpoon:list():select(2)
-    end,
-    { desc = 'Switch to harpoon window 2' }
-    )
-    vim.keymap.set('n', '<C-l>', function()
-      harpoon:list():select(3)
-    end,
-    { desc = 'Switch to harpoon window 3' }
-    )
-    vim.keymap.set('n', '<C-m>', function()
-      harpoon:list():select(4)
-    end,
-    { desc = 'Switch to harpoon window 4' }
-    )
-  end,
+  -- 'ThePrimeagen/harpoon',
+  -- branch = 'harpoon2',
+  -- dependencies = { 'nvim-lua/plenary.nvim' },
+  --
+  -- -- local harpoon = require("harpoon")
+  --
+  -- -- harpoon:setup()
+  -- config = function()
+  --   local harpoon = require 'harpoon'
+  --   harpoon:setup()
+  --
+  --   vim.keymap.set('n', '<C-h>', function()
+  --     harpoon:list():add()
+  --   end,
+  --   { desc = 'Add to harpoon' }
+  --   )
+  --   vim.keymap.set('n', '<leader>i', function()
+  --     harpoon.ui:toggle_quick_menu(harpoon:list())
+  --   end,
+  --   { desc = 'Harpoon menu' }
+  --   )
+  --
+  --   vim.keymap.set('n', '<C-j>', function()
+  --     harpoon:list():select(1)
+  --   end,
+  --   { desc = 'Switch to harpoon window 1' }
+  --   )
+  --   vim.keymap.set('n', '<C-k>', function()
+  --     harpoon:list():select(2)
+  --   end,
+  --   { desc = 'Switch to harpoon window 2' }
+  --   )
+  --   vim.keymap.set('n', '<C-l>', function()
+  --     harpoon:list():select(3)
+  --   end,
+  --   { desc = 'Switch to harpoon window 3' }
+  --   )
+  --   vim.keymap.set('n', '<C-m>', function()
+  --     harpoon:list():select(4)
+  --   end,
+  --   { desc = 'Switch to harpoon window 4' }
+  --   )
+  -- end,
 }
