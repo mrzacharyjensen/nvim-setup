7dcc2d4bf7153a59cd1bb49534d6b569fc4da4bd
Zachary Jensen <mrzacharyjensen@gmail.com>
2025-05-06 17:50:30 +1200

    Add macro for logging selected variable

--- lua/macros.lua
+++ lua/macros.lua
@@ -1,9 +1,11 @@
 -- Logging variable to console based on file type
--- Set to register 'l'
+-- Set to register 'l' for selecting word under cursor
+-- Set to register 'p' for already selected variable in visual mode
 vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
   pattern = { '*.js', '*.jsx', '*.ts', '*.tsx' },
   callback = function()
     vim.fn.setreg('l', 'viw"yyoconsole.log(""ypa: ", "ypa);')
+    vim.fn.setreg('p', '"yyoconsole.log(""ypa: ", "ypa);')
   end,
 })
 
@@ -11,6 +13,7 @@ vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
   pattern = '*.rs',
   callback = function()
     vim.fn.setreg('l', 'viw"yyoprintln!(""ypa: {"ypa:?}");')
+    vim.fn.setreg('p', '"yyoprintln!(""ypa: {"ypa:?}");')
   end,
 })
 
@@ -18,5 +21,6 @@ vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
   pattern = '*.py',
   callback = function()
     vim.fn.setreg('l', 'viw"yyoprint(f""ypa: {"ypa}")')
+    vim.fn.setreg('p', '"yyoprint(f""ypa: {"ypa}")')
   end,
 })
